d040b5517bf56a14cddf1598e00387ff7c322fca
caracosta <cgaglianocosta@kippnj.org>
2018-07-03 19:46:11 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,17 @@
-# Build your say_hello method here
+# # Build your say_hello method here
+
+# I call on the method, say_hello, and give it the string "Gabriela" 
+# say_hello("Gabriela")
+ 
+# The method prints this text to the screen:
+# Hello Gabriela
+
+
+
+
+def say_hello(name = "Ruby Programmer")
+ puts "Hello #{name}!"
+end
+
+
+
